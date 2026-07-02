8c2ce8f880557e47148be477f69dcaa2bed42209
mojtaba139797 <mojtabahabibi139797@gmail.com>
2026-07-02 10:53:28 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import zarbdar from "./assets/zarbdar.png"
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-base-200 flex items-center justify-center">
+      <img src={zarbdar} alt="" />
+    </div>
+  );
+}
+
+export default App;
